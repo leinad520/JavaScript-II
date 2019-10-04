4321358d0715287037b9a692c07e7c9e06f4a1a9
Daniel Sangyoon Kim <ksydaniel@gmail.com>
2019-10-03 20:36:31 -0700

    finish stretch

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,28 +58,43 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
+runners.forEach(obj => fullNames.push(`${obj.first_name} ${obj.last_name}`))
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
-let firstNamesAllCaps = [];
+let firstNamesAllCaps = runners.map(obj => obj.first_name.toUpperCase());
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = [];
+let runnersLargeSizeShirt = runners.filter(obj => obj.shirt_size === 'L');
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+let ticketPriceTotal = runners.reduce((acc, i) => acc + i.donation, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Filter for donations 200+
+let bigDonation = runners.filter(obj => obj.donation >= 200);
+console.log(bigDonation);
 
 // Problem 2
+// Order donations smallest to largest
+let donationSize = runners.sort((a,b) => a.donation - b.donation);
+console.log(donationSize);
 
 // Problem 3
+//convert everybodys emails into gmails, and put into array
+
+let splitEmails = runners.map(obj => obj.email.split('@'));
+
+splitEmails.forEach(arr => arr.pop())
+splitEmails.forEach(arr => arr.push("@gmail.com"));
+let gmails = splitEmails.map(arr => arr.join(''));
+console.log(gmails);
